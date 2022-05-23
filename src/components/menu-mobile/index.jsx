d5832dda9b01382dd5c01d1a470fb7b719182884
phvohieunghia <phvohieunghia@gmail.com>
2022-05-23 18:43:03 +0700

    finish sidebar mobile  event

--- src/components/menu-mobile/index.jsx
+++ src/components/menu-mobile/index.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import './style.scss'
 import globalSlice from 'reducers/global'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleMenu } from 'selectors'
 
 const Button = () => {
   const dispatch = useDispatch()
-  const [toggle, setToggle] = useState(false)
+  const state = useSelector(toggleMenu)
   function handleButton() {
-    setToggle(!toggle)
-    dispatch(globalSlice.actions.toggleMenu(!toggle))
+    dispatch(globalSlice.actions.toggleMenu(!state))
   }
   return (
     <div className="menu">
       <div
-        className={'nav__icon-soft' + (toggle ? ' icon-translate' : '')}
+        className={'nav__icon-soft' + (state ? ' icon-translate' : '')}
         onClick={handleButton}
       >
         <span></span>
--- src/components/sidebar-mobile/index.jsx
+++ src/components/sidebar-mobile/index.jsx
@@ -1,22 +1,47 @@
 import React from 'react'
 
 import './style.scss'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { toggleMenu } from 'selectors'
+import { NavLink } from 'react-router-dom'
+import globalSlice from 'reducers/global'
 
 const SidebarMobile = () => {
+  const dispatch = useDispatch()
+  function handleItem() {
+    dispatch(globalSlice.actions.toggleMenu(false))
+  }
   const state = useSelector(toggleMenu)
   return (
     <>
       {state && (
         <div className="sidebar-mobile">
           <div className="list">
-            <div className="item">Dashboard</div>
+            <NavLink onClick={handleItem} className="item" to="/">
+              Dashboard
+            </NavLink>
+            <NavLink onClick={handleItem} className="item" to="/egg-baskets">
+              Egg baskets
+            </NavLink>
+            <NavLink onClick={handleItem} className="item" to="/my-assets">
+              My Assets
+            </NavLink>
+            <NavLink onClick={handleItem} className="item" to="/marketplace">
+              Marketplace
+            </NavLink>
+            <NavLink onClick={handleItem} className="item" to="/staking">
+              Staking
+            </NavLink>
+            <NavLink onClick={handleItem} className="item" to="/vesting">
+              Vesting
+            </NavLink>
+
+            {/* <div className="item">Dashboard</div>
             <div className="item">Egg baskets</div>
             <div className="item">My Assets</div>
             <div className="item">Marketplace</div>
             <div className="item">Staking</div>
-            <div className="item">Vesting</div>
+            <div className="item">Vesting</div> */}
           </div>
         </div>
       )}
