d4e02c0832f5b61525dbe74cf568dd9339a4c561
phvohieunghia <phvohieunghia@gmail.com>
2022-05-25 18:11:30 +0700

    up

--- src/utils/metamask.js
+++ src/utils/metamask.js
@@ -1,6 +1,14 @@
-export function isMetamask() { 
-    if(window.ethereum) {
-        return true;
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+export async function connect() {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex)
     }
-    else return false;
-}
+  }
